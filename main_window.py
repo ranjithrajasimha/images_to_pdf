8f0b663c523a0a68e07a713816ecef41a6fe19f1
ranjithrajasimha <ranjithrajasimha@gmail.com>
2020-02-10 15:43:35 +0100

    Resize the default window

--- main_window.py
+++ main_window.py
@@ -52,7 +52,7 @@ if __name__ == "__main__":
     app = QtWidgets.QApplication([])
 
     widget = MyWidget()
-    widget.resize(800, 600)
+    widget.resize(400, 300)
     widget.show()
 
     sys.exit(app.exec_())
